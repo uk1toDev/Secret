de9a0ec5b6776138b8bbef2025672e2cfcea3b52
Abel Cabrera <uk1todev@gmail.com>
2020-07-02 17:58:39 +0200

    Md5 encrypt

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -20,10 +18,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +37,7 @@ app.post("/register", function(req, res){
 
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -60,7 +54,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
